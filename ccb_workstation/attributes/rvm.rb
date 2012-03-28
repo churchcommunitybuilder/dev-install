62d0795ca7b5fa0a8d7409784f94f2ae80973708
Kenneth Scott <kscott@churchcommunitybuilder.com>
2012-03-28 16:20:41 -0600

    Add ruby version for bbt

--- ccb_workstation/attributes/rvm.rb
+++ ccb_workstation/attributes/rvm.rb
@@ -5,6 +5,7 @@ node.override["rvm"]= {
     #"ree-1.8.7-2012.02" => { :env => "CC=gcc-4.2" },
     #"ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
     #"ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
+    "ruby-1.9.2-p180" => {},
     "ruby-1.9.3-p0" => { :command_line_options => "--with-gcc=clang" },
   },
   "default_ruby" => "ruby-1.9.3-p0"
