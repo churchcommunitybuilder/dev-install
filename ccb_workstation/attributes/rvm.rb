e3d76a17383b7b86f7e5b2b969e25db227c51741
Kenneth Scott <kscott@churchcommunitybuilder.com>
2012-03-28 16:49:27 -0600

    Just override the rubies portion

--- ccb_workstation/attributes/rvm.rb
+++ ccb_workstation/attributes/rvm.rb
@@ -1,12 +1,3 @@
-node.override["rvm"]= {
-  "rubies" => {
-      # gcc-4.2 is available from the osx-gcc project on github. We dont install it
-      # as it overwrites later versions of files provided by xcode.
-    #"ree-1.8.7-2012.02" => { :env => "CC=gcc-4.2" },
-    #"ruby-1.8.7-p358" => { :env => "CC=gcc-4.2" },
-    #"ruby-1.9.2-p290" => { :env => "CC=gcc-4.2" },
-    "ruby-1.9.2-p180" => {},
-    "ruby-1.9.3-p0" => { :command_line_options => "--with-gcc=clang" },
-  },
-  "default_ruby" => "ruby-1.9.3-p0"
+node.override["rvm"]["rubies"] = {
+  "ruby-1.9.2-p180" => { :command_line_options => "--with-gcc=clang" }
 }
