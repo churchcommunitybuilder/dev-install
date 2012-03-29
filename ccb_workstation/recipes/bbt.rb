bfa5a7b47ac7e87efcf0f6984b88809948d6bcba
Kenneth Scott <kscott@churchcommunitybuilder.com>
2012-03-29 14:30:54 -0600

    Create user's source directory if needed

--- ccb_workstation/recipes/bbt.rb
+++ ccb_workstation/recipes/bbt.rb
@@ -2,6 +2,11 @@ include_recipe "ccb_workstation::rvm"
 include_recipe "ccb_workstation::gearman"
 include_recipe "ccb_workstation::mysql"
 
+directory "#{WS_HOME}/CCB/src" do
+  owner WS_USER
+  action :create
+end
+
 git "CCB/src/bbt" do
   repository "git@testdrivesite.beanstalkapp.com:/bbt.git"
   branch "master"
