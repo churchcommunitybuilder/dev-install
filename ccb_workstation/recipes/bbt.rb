e252b7023d735fc5c79e485b400a1e743f887cd4
Kenneth Scott <kscott@churchcommunitybuilder.com>
2012-03-29 14:28:30 -0600

    Add checkout of bbt code

--- ccb_workstation/recipes/bbt.rb
+++ ccb_workstation/recipes/bbt.rb
@@ -1,3 +1,13 @@
 include_recipe "ccb_workstation::rvm"
 include_recipe "ccb_workstation::gearman"
 include_recipe "ccb_workstation::mysql"
+
+git "CCB/src/bbt" do
+  repository "git@testdrivesite.beanstalkapp.com:/bbt.git"
+  branch "master"
+  destination "#{WS_HOME}/CCB/src/bbt"
+  action :sync
+  user WS_USER
+end
+
+
