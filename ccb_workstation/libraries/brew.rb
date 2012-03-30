3817a8889cb925133f439565191e7788ecacb451
Kenneth Scott <kscott@churchcommunitybuilder.com>
2012-03-30 15:53:55 -0600

    Clean up brew commands

--- ccb_workstation/libraries/brew.rb
+++ ccb_workstation/libraries/brew.rb
@@ -1,48 +1,49 @@
 class Chef::Recipe
+  BREW_CMD = "/usr/local/bin/brew"
+
   def brew_install(package, opts={})
     include_recipe "ccb_workstation::homebrew"
 
     if brew_installed?(package) && (brew_outdated?(package) || brew_has_multiple_versions_installed?(package))
-      execute "brew remove --force #{package} && brew install #{package} #{opts[:brew_args]}" do
+      execute "#{BREW_CMD} remove --force #{package} && #{BREW_CMD} install #{package} #{opts[:brew_args]}" do
         user WS_USER
       end
     end
 
     unless brew_installed?(package)
-      execute "brew install #{package} #{opts[:brew_args]}" do
+      execute "#{BREW_CMD} install #{package} #{opts[:brew_args]}" do
         user WS_USER
       end
     end
   end
 
   def brew_outdated?(package)
-    outdated=system("brew outdated | grep -q #{package}")
-    Chef::Log.debug("brew package #{package} " + (outdated ? "IS" : "IS NOT") + " outdated.") 
+    outdated=system("#{BREW_CMD} outdated | grep -q #{package}")
+    Chef::Log.debug("#{BREW_CMD} package #{package} " + (outdated ? "IS" : "IS NOT") + " outdated.") 
     outdated
   end
 
   def brew_installed?(package)
     include_recipe "ccb_workstation::homebrew"
 
-    installed=(system("brew list #{package} > /dev/null") || brew_has_multiple_versions_installed?(package))
-    Chef::Log.debug("brew package #{package} " + (installed ? "IS" : "IS NOT") + " installed.")
+    installed=(system("#{BREW_CMD} list #{package} > /dev/null") || brew_has_multiple_versions_installed?(package))
+    Chef::Log.debug("#{BREW_CMD} package #{package} " + (installed ? "IS" : "IS NOT") + " installed.")
     installed
   end
   
   def brew_has_multiple_versions_installed?(package)
-    multiple=system("brew list #{package} | grep -q '#{package} has multiple installed versions'")
-    Chef::Log.debug("brew package #{package} " + (multiple ? "HAS" : "does NOT HAVE") + " multiple versions.") 
+    multiple=system("#{BREW_CMD} list #{package} | grep -q '#{package} has multiple installed versions'")
+    Chef::Log.debug("#{BREW_CMD} package #{package} " + (multiple ? "HAS" : "does NOT HAVE") + " multiple versions.") 
     multiple
   end
 
   def brew_remove(package)
     include_recipe "ccb_workstation::homebrew"
 
-    brew_installed = `brew list | grep #{package}`
-    unless brew_installed.empty?
-      execute "brew remove #{package}" do
+    if brew_installed? package
+      execute "#{BREW_CMD} remove #{package}" do
         user WS_USER
-        command "brew remove #{package}"
+        command "#{BREW_CMD} remove #{package}"
       end
     end
   end
@@ -50,18 +51,18 @@ class Chef::Recipe
   def brew_update
     include_recipe "ccb_workstation::homebrew"
 
-    execute "brew update" do
+    execute "#{BREW_CMD} update" do
       user WS_USER
-      command "brew update"
+      command "#{BREW_CMD} update"
     end
   end
 
   def brew_linkapps
     include_recipe "ccb_workstation::homebrew"
 
-    execute "brew linkapps" do
+    execute "#{BREW_CMD} linkapps" do
       user WS_USER
-      command "brew linkapps"
+      command "#{BREW_CMD} linkapps"
     end
   end
 end
